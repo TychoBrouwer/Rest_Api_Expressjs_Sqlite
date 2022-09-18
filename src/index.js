8504ff91f2837073eba5993aa08e14443b2bbf96
TychoBrouwer <tychobrouwer33@gmail.com>
2022-09-18 11:37:53 +0200

    auto commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const signInRouter = require('./routes/users');
+import express, { json } from 'express';
+import signInRouter from './routes/users';
 
 const port = 3000 || process.env.PORT;
 
 const app = express();
-app.use(express.json());
+app.use(json());
 
 app.get('/', (req, res) => {
   res.json({ message: 'alive' });
