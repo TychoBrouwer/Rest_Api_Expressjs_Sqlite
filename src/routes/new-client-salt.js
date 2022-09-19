abf2ca897ffe9f8350ae6c376f5db4f96dd2ef6e
TychoBrouwer <tycho.tbrouwer@gmail.com>
2022-09-19 12:38:36 +0200

    auto commit

--- src/routes/new-client-salt.js
+++ src/routes/new-client-salt.js
@@ -5,7 +5,7 @@ const router = Router();
 
 router.post('/', (req, res, next) => {
   try {
-    res.json(newClientSalt.get(req.body));
+    res.json(newClientSalt.set(req.body));
   } catch (err) {
     console.error('Error while getting login ', err.message);
     next(err);
--- src/services/new-client-salt.js
+++ src/services/new-client-salt.js
@@ -3,17 +3,22 @@ const bcrypt = require('bcrypt');
 const saltGen = require('../utils/salt-gen');
 const sanitizeInput = require('../utils/sanitize-input');
 
-function get(data) {
+function set(data) {
   const { email } = sanitizeInput(data);
 
   const salt = bcrypt.genSaltSync(10);
-  const result = saltGen.setSalt(true, salt, email);
 
-  console.log(result, salt);
+  try {
+    const result = saltGen.setSalt(true, salt, email);
+
+    console.log(result, salt);
+  } catch (error) {
+    return false;
+  }
 
   return salt;
 }
 
 module.exports = {
-  get,
+  set,
 };
