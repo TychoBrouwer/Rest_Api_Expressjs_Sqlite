20c537a8ce9dca1b0b62c739fc7e47af193fe480
TychoBrouwer <tycho.tbrouwer@gmail.com>
2022-09-19 09:49:02 +0200

    auto commit

--- src/utils/validate-email.js
+++ src/utils/validate-email.js
@@ -4,4 +4,4 @@ const validateEmail = (email) => String(email)
     /^(([^<>()[\]\\.,;:\s@']+(\.[^<>()[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
   );
 
-export default validateEmail;
+module.exports = validateEmail;
