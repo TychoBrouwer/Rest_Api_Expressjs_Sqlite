f62104f1d66c8fb9222854454a991141032b22ea
TychoBrouwer <tycho.tbrouwer@gmail.com>
2022-09-19 11:42:46 +0200

    auto commit

--- src/utils/salt-gen.js
+++ src/utils/salt-gen.js
@@ -8,7 +8,7 @@ function getSalt(clientSalt, email) {
   }
 
   const query = `SELECT ${value} FROM login_table WHERE email = ?`;
-  const salt = db.all(query, email);
+  const salt = db.query(query, email);
 
   return salt;
 }
