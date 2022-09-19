e251fcd2209eabd6f5403a7ba944413ff7c66daf
TychoBrouwer <tycho.tbrouwer@gmail.com>
2022-09-19 20:25:51 +0200

    auto commit

--- src/utils/init-database.js
+++ src/utils/init-database.js
@@ -4,7 +4,7 @@ function initDatabase() {
   const loginTable = `
     CREATE TABLE IF NOT EXISTS 
     login_table (
-      'user_id' int NOT NULL, 
+      'user_id' int AUTOINCREMENT, 
       'email' varchar NOT NULL, 
       'password' varchar, 
       'client_salt' varchar, 
--- test_api.py
+++ test_api.py
@@ -2,25 +2,25 @@ import requests
 
 # url = 'http://192.168.178.142:3000/new-client-salt'
 # data = {
-#   'email': 'tychdadadaoffaf@nfrg.vrs',
+#   'email': 'adadaoffaf@nfrg.vrs',
 # }
 # $2b$10$S2lBs57VaKGeykpGTVLwuO
 
 # url = 'http://192.168.178.142:3000/sign-up'
 # data = {
-#   'email': 'tychdadadaoffaf@nfrg.vrs',
+#   'email': 'adadaoffaf@nfrg.vrs',
 #   'password': 'gferwfregfg',
 # }
 
 # url = 'http://192.168.178.142:3000/get-client-salt'
 # data = {
-#   'email': 'tychdadadaoffaf@nfrg.vrs',
+#   'email': 'adadaoffaf@nfrg.vrs',
 # }
 # $2b$10$m5R4T6qk7nU/slLHNxWgUO
 
 url = 'http://192.168.178.142:3000/sign-in'
 data = {
-  'email': 'tychdadadaoffaf@nfrg.vrs',
+  'email': 'adadaoffaf@nfrg.vrs',
   'password': 'gferwfregfg',
 }
 
