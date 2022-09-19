6f9d7815c3c3d80875c1b2c375de7bd67dbd593f
TychoBrouwer <tycho.tbrouwer@gmail.com>
2022-09-19 19:29:33 +0200

    auto commit

--- test_api.py
+++ test_api.py
@@ -4,23 +4,19 @@ import requests
 # data = {
 #   'email': 'trgrg@nfegrg.vrs',
 # }
+# $2b$10$YupWWGUWPKT4UePVbzA.qO
 
-# url = 'http://192.168.178.142:3000/temp-salt'
-# data = {
-#   'email': 'trgrg@nfegrg.vrs',
-# }
+url = 'http://192.168.178.142:3000/sign-up'
+data = {
+  'email': 'trgrg@nfegrg.vrs',
+  'password': 'gferwfregfg',
+}
 
-# url = 'http://192.168.178.142:3000/sign-up'
+# url = 'http://192.168.178.142:3000/sign-in'
 # data = {
 #   'email': '1111111111@nfegrsdsdg.vrs',
 #   'password': 'gregfg',
 # }
-
-url = 'http://192.168.178.142:3000/sign-in'
-data = {
-  'email': '1111111111@nfegrsdsdg.vrs',
-  'password': 'gregfg',
-}
 
 x = requests.post(url, json = data)
 
