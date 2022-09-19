d91246b810080087091588058a3c915e40aefbec
TychoBrouwer <tycho.tbrouwer@gmail.com>
2022-09-19 10:57:23 +0200

    auto commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
-const signInRouter = require('./routes/users');
+const signInRouter = require('./routes/sign-in');
+const signUpRouter = require('./routes/sign-up');
+const getClientSalt = require('./routes/get-client-salt');
 const initDatabase = require('./utils/init-database');
 
 const port = 3000 || process.env.PORT;
@@ -15,6 +17,8 @@ app.get('/', (req, res) => {
 });
 
 app.use('/sign-in', signInRouter);
+app.use('/sign-up', signUpRouter);
+app.use('/get-client-salt', getClientSalt);
 
 app.listen(port, () => {
   console.log(`Api app listening on port ${port}`);
