baa3c2f568d306f35b666ab1069ad5d288edcfcf
TychoBrouwer <tycho.tbrouwer@gmail.com>
2022-09-19 11:17:03 +0200

    auto commit

--- src/utils/init-database.js
+++ src/utils/init-database.js
@@ -4,20 +4,8 @@ const { resolve } = require('path');
 function initDatabase() {
   const db = new Sqlite(resolve('users.db'), { fileMustExist: true });
 
-  // const createTable = `
-  //   CREATE TABLE IF NOT EXISTS 
-  //   login_table (
-  //     'user_id' int NOT NULL, 
-  //     'email' varchar NOT NULL, 
-  //     'password' varchar, 
-  //     'client_salt' varchar, 
-  //     'server_salt' varchar, 
-  //     PRIMARY KEY (user_id),
-  //     UNIQUE(email)
-  //   );`;
-
   const createTable = `
-    UPDATE TABLE 
+    CREATE TABLE IF NOT EXISTS 
     login_table (
       'user_id' int NOT NULL, 
       'email' varchar NOT NULL, 
