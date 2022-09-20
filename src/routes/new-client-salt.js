0ebe798776f83b39aba1dbcb97d0ef3d0f013861
TychoBrouwer <tychobrouwer33@gmail.com>
2022-09-20 14:12:56 +0200

    auto commit

--- src/routes/new-client-salt.js
+++ src/routes/new-client-salt.js
@@ -5,6 +5,7 @@ const router = Router();
 
 router.post('/', (req, res, next) => {
   try {
+    console.log('test');
     res.json(newClientSalt.set(req));
   } catch (err) {
     console.error('Error while getting login ', err.message);
