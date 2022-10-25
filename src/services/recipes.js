40cfdd1a3b42b50e8e8c8a6fcb41d4ffc1e4ca7f
TychoBrouwer <tychobrouwer33@gmail.com>
2022-10-25 13:28:07 +0200

    test limit

--- src/services/recipes.js
+++ src/services/recipes.js
@@ -6,11 +6,12 @@ function searchIngredientRecipe({ ingredients, limit }) {
     FROM recipes_ingredients
     JOIN ingredients ON recipes_ingredients.fk_ingredient = ingredients.ingredient_ID
     JOIN recipes ON recipes_ingredients.fk_recipe = recipes.recipe_ID
+    LIMIT ?
     WHERE 
   `;
   console.log(ingredients);
 
-  for (let i = 0; i < Math.min(ingredients.length, limit); i += 1) {
+  for (let i = 0; i < ingredients.length; i += 1) {
     console.log(ingredients.length);
     query += 'ingredients.ingredient_name = ? ';
   }
@@ -20,7 +21,7 @@ function searchIngredientRecipe({ ingredients, limit }) {
   let queryResult;
 
   try {
-    queryResult = db.query(query, ingredients);
+    queryResult = db.query(query, [limit, ...ingredients]);
     console.log(queryResult);
 
     if (!queryResult[0]) {
